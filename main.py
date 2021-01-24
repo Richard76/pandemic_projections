ee854e501ba7c1cca74601e419f00e3d2795db57
Richard76 <richardfarr7@gmail.com>
2021-01-24 00:03:54 +0000

    getting closer

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ usa_infections_daily = 192000 # yesterdays infections
 # daily vaccinated
 usa_vaccinations_daily = 975540 # yesterdays shots https://www.nytimes.com/interactive/2020/us/covid-19-vaccine-doses.html
 usa_vaccinated_daily = usa_vaccinations_daily / 2
+vacc_rate_increase = 0 # percent that the daily number of vaccinations increases by
+usa_vaccinated_daily = usa_vaccinated_daily * (1 + (vacc_rate_increase / 100))
 
 # % of people needing to be infected or vaccinated for pandemic to be over
 usa_infections_vaccinations_needed = 0.90 
@@ -62,11 +64,12 @@ print(f"{usii} people have either been infected or vaccinated so far")
 
 # 2. Tell people what percentage of the population that is
 pct_of_population = round( 100 * usa_population_infected_immune / usa_population)
-print(f"That's {pct_of_population} % of the popluation infected or vaccinated so far")
+print(f"That's only {pct_of_population} % of the popluation infected or vaccinated as of {today}")
+print(" ")
 
 # 3. Tell people what percentage of the population needs to be infected or vaccinated for the pandemic to end
 usa_ivn = round(100 * usa_infections_vaccinations_needed)
-print(f"We need {usa_ivn} % of the population to be infected or vaccinated for the pandemic to be over")
+print(f"If we need {usa_ivn} % of the population to be infected or vaccinated for the pandemic to be over...")
 
 # 4. Calculate the Total # People that need to be infected or vaccinated for the pandemic to be over
 usa_population_needed = round(usa_infections_vaccinations_needed * usa_population)
@@ -87,11 +90,11 @@ print(f"There are {ivpd} people getting infected or vaccinated per day")
 # 6. Estimate how many days it will take at this rate
 days_until_pandemic_over = round(people_left / infected_or_vaccinated_per_day)
 dupo = "{:,}".format(days_until_pandemic_over)
-print(f"That means it will be {dupo} days until the pandemic is over")
+print(f"That means it should only be {dupo} days until the pandemic is over")
 
 # 7. Add the projected number of days to today's date to get the date the pandemic will end
 date_pandemic_over = today + datetime.timedelta(days=days_until_pandemic_over)
-print(f"The Date that the Pandemic is expected to be over is: {date_pandemic_over}")
+print(f"The Pandemic should be done by: {date_pandemic_over}")
 
 
 # Goal 2: Estimate how many people infected, dead, vaccinated, & untouched
@@ -102,30 +105,32 @@ print("Goal 2: Estimate how many people infected, vaccinated, & untouched?")
 # 1. How many will be infected by the end of the pandemic?
 usa_infected_proj = round(usa_infected + (days_until_pandemic_over * usa_infections_daily))
 usa_ip = "{:,}".format(usa_infected_proj)
-print(f"We project {usa_ip} total people infected in the USA by {date_pandemic_over}")
+print(f"We project that a total of {usa_ip} people will have been infected in the USA by {date_pandemic_over}")
 
 
 # 2. How many people will be vaccinated by the end of the pandemic?
 usa_vaccinated_proj = round(usa_vaccinated + days_until_pandemic_over * usa_vaccinated_daily)
 usa_vp = "{:,}".format(usa_vaccinated_proj)
-print(f"We project {usa_vp} total people vaccinated in the USA by {date_pandemic_over}")
+print(f"We project that {usa_vp} total people will be vaccinated in the USA by {date_pandemic_over}")
 
 # 3. How many people dead so far
 dead_so_far = round(usa_infected * virus_mortality_rate)
 usa_dsf = "{:,}".format(dead_so_far)
-print(f"We estimate the USA to have had {usa_dsf} deaths so far")
+print(f"{usa_dsf} people in the US have died so far.")
 
 
 # 4. How many people projected to die
 usa_dead_projected = round(usa_infected_proj * virus_mortality_rate)
 usa_dp = "{:,}".format(usa_dead_projected)
-print(f"We estimate the USA to have {usa_dp} deaths by {date_pandemic_over} ")
+print(f"We estimate that the USA will have {usa_dp} deaths by {date_pandemic_over} ")
 
 
 # 5. How many people more to die
 usa_additional_deaths = usa_dead_projected - usa_deaths
 usa_ad = "{:,}".format(usa_additional_deaths)
-print(f"We estimate {usa_ad} additional deaths in the US over the next {days_until_pandemic_over} days ")
+print(f"That means there will be {usa_ad} additional deaths in the US over the next {days_until_pandemic_over} days ")
+deaths_per_day_proj = round(usa_additional_deaths / days_until_pandemic_over)
+print(f"That translates to an average of {deaths_per_day_proj} deaths every day until then.")
 
 # 6. How many people will be untouched by the end of the pandemic?
 usa_people_untouched_proj = round(usa_population - usa_infected_proj - usa_vaccinated_proj)
@@ -136,6 +141,26 @@ print(f"We project {usa_up} total people untouched by the virus or a vaccine in 
 # print(usa_population == usa_infected_proj + usa_vaccinated_proj + usa_people_untouched_proj)
 # print(usa_population)
 
+
+
+# Goal 3 - What are your odds?
+
+print(f"As of {today}, here are your odds going forward")
+
+# 1. Chance of being vaccinated
+odds_vaccinated = 0.3
+
+# 2. Chance of not being vaccinated and not being infected
+odds_other = 0.2
+
+# 3. Chance of getting infected and not dying
+odds_infected = 0.5
+odds_infected_live = round(100 * (odds_infected * (1 - virus_mortality_rate)), 2)
+print(f"Your chances of getting infected and not dying are {odds_infected_live} % ")
+
+# 4. Change of getting infected and dying
+odds_infected_death = round(100 * (odds_infected * virus_mortality_rate), 2)
+print(f"Your chances of getting infected and dying are {odds_infected_death} % ")
 
 
 #### Part 2 - Make the model Better
